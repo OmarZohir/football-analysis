de001e118682d795b36491e4680f976d7e07788c
OmarZohir <misr1@hotmail.com>
2024-05-02 01:01:25 +0800

    Annotating the ball with an inverted triangle

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -5,6 +5,7 @@ import os
 import cv2
 import sys
 sys.path.append('../')
+import numpy as np
 from utils import get_center_of_bbox, get_bbox_width
 
 class Tracker:
@@ -83,14 +84,14 @@ class Tracker:
         return tracks        
     
     #draw an ellipse around the player, at the bottom of the bounding box
-    def draw_ellipse(self, frame, bbox, color, track_id):
+    def draw_ellipse(self, frame, bbox, color, track_id=None):
         y2 = int(bbox[3])
         x_center, _ = get_center_of_bbox(bbox)
         width = get_bbox_width(bbox)
         
         frame = cv2.ellipse(frame,
                             center=(x_center, y2),
-                            axes=(int(width), int(0.25*width)),
+                            axes=(int(width), int(0.35*width)),
                             angle=0,
                             startAngle=-45,
                             endAngle=235,
@@ -98,8 +99,48 @@ class Tracker:
                             thickness=1,
                             lineType=cv2.LINE_4) 
         
+        #draw a rectangle below the player showing the player number
+        #TODO: add player speed tracking
+        rectangle_width = 40
+        rectangle_height = 20
+        x1_rect = x_center - rectangle_width//2
+        x2_rect = x_center + rectangle_width//2
+        y1_rect = y2 - rectangle_height//2 + 15
+        y2_rect = y2 + rectangle_height//2 + 15
+    
+        if track_id is not None:
+            cv2.rectangle(frame,
+                          (int(x1_rect), int(y1_rect)),
+                          (int(x2_rect), int(y2_rect)),
+                          color, 
+                          cv2.FILLED)
+            
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -=  10
+                
+            cv2.putText(frame,
+                        f"{track_id}",
+                        (int(x1_text), int(y1_rect+15)),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.6,
+                        (0, 0, 0),
+                        2)
         return frame
     
+    def draw_triangle(self, frame, bbox, color):
+        y = int(bbox[1])
+        x, _ = get_center_of_bbox(bbox)
+        #assume the ball would fit in an inverted isosceles triangle with a base of 20 pixels
+        triangle_points = np.array([
+            [x,y],
+            [x-10, y-20],
+            [x+10, y+20],
+        ])
+        cv2.drawContours(frame, [triangle_points], 0, color, cv2.FILLED)
+        #draw a black border
+        cv2.drawContours(frame, [triangle_points], 0, (0,0,0), cv2.FILLED)
+        
     def draw_annotations(self, frames, tracks):
         output_frames = []
         frames_copy = frames.copy()
@@ -114,14 +155,13 @@ class Tracker:
             
             #Drawing referees
             for track_id, referee_bbox in referee_dict.items():
-                frame = self.draw_ellipse(frame, referee_bbox, (0, 255, 255), track_id)
+                frame = self.draw_ellipse(frame, referee_bbox, (0, 255, 255))
             
             #Drawing ball
             for track_id, ball_bbox in ball_dict.items():
-                frame = self.draw_ellipse(frame, ball_bbox, (255, 0, 255), track_id)
+                frame = self.draw_triangle(frame, ball_bbox, (255, 0, 255))
                 
             output_frames.append(frame)
-            del frame
             
         return output_frames
     
